ce4ba7b45c3b480a5525a5aa4c2a63e04f9dc229
Daria Sopshina <dasha.sopshina@mail.ru>
2026-01-22 20:26:42 +0500

    Update build for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/spacex-launches-2020/',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
 });

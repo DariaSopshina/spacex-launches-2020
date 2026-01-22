28b98821ee291c50da09514326a25cb6c2058181
Daria Sopshina <dasha.sopshina@mail.ru>
2026-01-22 20:17:28 +0500

    Move reducer tests to store/launches

--- src/store/launches/reducer.test.ts
+++ src/store/launches/reducer.test.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest';
 import { reducer } from './reducer';
-import type { State, Launch } from './types';
+import type { State, Launch } from '../../types';
 
 const initialState: State = {
   launches: [],
